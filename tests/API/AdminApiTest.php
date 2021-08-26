84cb05012ee6fa4f181c8c05ed286723c4de600d
Mateusz <mateusz@qunabu.com>
2021-08-26 18:29:45 +0200

    truncate

--- tests/API/AdminApiTest.php
+++ tests/API/AdminApiTest.php
@@ -30,9 +30,9 @@ class AdminApiTest extends TestCase
         $this->user = config('auth.providers.users.model')::factory()->create();
         $this->user->guard_name = 'api';
         $this->user->assignRole(UserRole::ADMIN);
-        // OrderItem::truncate();
-        Order::truncate();
-        Course::truncate();
+        OrderItem::query()->delete();
+        Order::query()->delete();
+        Course::query()->delete();
     }
 
     public function test_list_orders()
