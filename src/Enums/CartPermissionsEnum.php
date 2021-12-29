e8a9869f88603c036f07561665940abdbbb20769
KrzysztofDziedziechEscolasoft <96292232+KrzysztofDziedziechEscolasoft@users.noreply.github.com>
2021-12-29 12:29:53 +0100

    #94 - permissions renamed (#32)
    Co-authored-by: KD <do>

--- src/Enums/CartPermissionsEnum.php
+++ src/Enums/CartPermissionsEnum.php
@@ -6,6 +6,6 @@ use EscolaLms\Core\Enums\BasicEnum;
 
 class CartPermissionsEnum extends BasicEnum
 {
-    const LIST_ALL_ORDERS = 'list orders';
-    const LIST_AUTHORED_COURSE_ORDERS = 'list authored orders';
+    const LIST_ALL_ORDERS = 'cart_order_list';
+    const LIST_AUTHORED_COURSE_ORDERS = 'cart_order_list-owned';
 }
