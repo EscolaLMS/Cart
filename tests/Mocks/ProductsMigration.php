0b216ba42fb870c421a4f589817c66561ac12d25
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2022-02-15 19:27:13 +0100

    Use temporary table, because create/drop table implicitly closes transaction

--- tests/Mocks/ProductsMigration.php
+++ tests/Mocks/ProductsMigration.php
@@ -2,6 +2,7 @@
 
 namespace EscolaLms\Cart\Tests\Mocks;
 
+use DB;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -9,21 +10,17 @@ class ProductsMigration
 {
     public static function run()
     {
-        if (Schema::hasTable('test_products')) {
-            Schema::drop('test_products');
-        }
-        if (Schema::hasTable('test_products_users')) {
-            Schema::drop('test_products_users');
-        }
-        Schema::create('test_products', function (Blueprint $table) {
-            $table->id();
+        DB::statement('DROP TEMPORARY TABLE IF EXISTS test_products');
+        DB::statement('DROP TEMPORARY TABLE IF EXISTS test_products_users');
+        DB::statement('CREATE TEMPORARY TABLE test_products (`id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))');
+        Schema::table('test_products', function (Blueprint $table) {
             $table->string('name');
             $table->unsignedInteger('price');
             $table->unsignedInteger('tax_rate')->default(0);
             $table->timestamps();
         });
-        Schema::create('test_products_users', function (Blueprint $table) {
-            $table->id();
+        DB::statement('CREATE TEMPORARY TABLE test_products_users (`id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))');
+        Schema::table('test_products_users', function (Blueprint $table) {
             $table->foreignId('test_product_id');
             $table->foreignId('user_id');
         });
