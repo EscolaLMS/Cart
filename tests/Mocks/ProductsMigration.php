ea4a602cd0af2180c21e46b3967fc6d596469ccd
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2022-02-15 19:39:52 +0100

    Creating tables during testing was stupid idea

--- tests/Mocks/ProductsMigration.php
+++ tests/Mocks/ProductsMigration.php
@@ -3,6 +3,8 @@
 namespace EscolaLms\Cart\Tests\Mocks;
 
 use DB;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -10,19 +12,27 @@ class ProductsMigration
 {
     public static function run()
     {
-        DB::statement('DROP TEMPORARY TABLE IF EXISTS test_products');
-        DB::statement('DROP TEMPORARY TABLE IF EXISTS test_products_users');
-        DB::statement('CREATE TEMPORARY TABLE test_products (`id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))');
-        Schema::table('test_products', function (Blueprint $table) {
-            $table->string('name');
-            $table->unsignedInteger('price');
-            $table->unsignedInteger('tax_rate')->default(0);
-            $table->timestamps();
-        });
-        DB::statement('CREATE TEMPORARY TABLE test_products_users (`id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))');
-        Schema::table('test_products_users', function (Blueprint $table) {
-            $table->foreignId('test_product_id');
-            $table->foreignId('user_id');
+        DB::transaction(function () {
+            if (DB::connection() instanceof MySqlConnection) {
+                DB::statement('DROP TEMPORARY TABLE IF EXISTS test_products');
+                DB::statement('DROP TEMPORARY TABLE IF EXISTS test_products_users');
+            }
+            if (DB::connection() instanceof PostgresConnection) {
+                DB::statement('DROP TABLE IF EXISTS test_products');
+                DB::statement('DROP TABLE IF EXISTS test_products_users');
+            }
+            DB::statement('CREATE TEMPORARY TABLE test_products (`id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))');
+            Schema::table('test_products', function (Blueprint $table) {
+                $table->string('name');
+                $table->unsignedInteger('price');
+                $table->unsignedInteger('tax_rate')->default(0);
+                $table->timestamps();
+            });
+            DB::statement('CREATE TEMPORARY TABLE test_products_users (`id` int(10) unsigned NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`))');
+            Schema::table('test_products_users', function (Blueprint $table) {
+                $table->foreignId('test_product_id');
+                $table->foreignId('user_id');
+            });
         });
     }
 }
