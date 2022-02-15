9ff0213be1cbcecdad922e09a793aae8cd31eb3d
Paweł Chołaj <pawel.cholaj@escolasoft.com>
2022-02-15 17:51:01 +0100

    Remove comments

--- src/Support/Helper.php
+++ src/Support/Helper.php
@@ -9,18 +9,11 @@ class Helper
 {
     public static function hasRelation(Model $model, string $key)
     {
-        // If the key already exists in the relationships array, it just means the
-        // relationship has already been loaded, so we'll just return it out of
-        // here because there is no need to query within the relations twice.
         if ($model->relationLoaded($key)) {
             return true;
         }
 
-        // If the "attribute" exists as a method on the model, we will just assume
-        // it is a relationship and will load and return results from the query
-        // and hydrate the relationship's value on the "relationships" array.
         if (method_exists($model, $key)) {
-            //Uses PHP built in function to determine whether the returned object is a laravel relation
             return is_a($model->$key(), Relation::class);
         }
 
