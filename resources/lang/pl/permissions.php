17fead09ae20331bc4e58e9504b42201c117f173
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 11:06:38 +0100

    Add translations

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+use EscolaLms\Cart\Enums\CartPermissionsEnum;
+
+return [
+    CartPermissionsEnum::LIST_ALL_ORDERS => 'Lista zamówień',
+    CartPermissionsEnum::LIST_ALL_PRODUCTS => 'Lista produktów',
+    CartPermissionsEnum::MANAGE_PRODUCTS => 'Zarządzanie produktami',
+    CartPermissionsEnum::LIST_PURCHASABLE_PRODUCTS => 'Lista produktów do kupienia',
+    CartPermissionsEnum::BUY_PRODUCTS => 'Kup produkt',
+];
